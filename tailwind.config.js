675cb8be4d253587c2c39d54e10a9bbffe3ca082
moamineayadi <mohamedamine.ayadi@medtech.tn>
2024-05-14 10:32:32 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["forest", "cupcake"]
+  }
+}
+
